18b8fdf8b00dbac5719e73f81598bc875e5eda15
songchuancai <songchuancai@qq.com>
2023-05-30 09:01:23 +0800

    增加Json字符串相关操作

--- src/main/java/com/scc/toolutils/json/JsonUtils.java
+++ src/main/java/com/scc/toolutils/json/JsonUtils.java
@@ -1,9 +1,15 @@
 package com.scc.toolutils.json;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author : scc
@@ -21,7 +27,49 @@ public class JsonUtils {
         // 配置时间序列（不使用时间序列）
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
     }
+    
+    // 测试用例--> Json字符串中有某个对象的值为集合，并且集合类型为Object
+    public void test1(){
+        JSONObject jsonObject = new JSONObject();
+        List<Object> datas = new ArrayList<>();
+        datas.add("2022-03-01");
+        datas.add("实际出勤人工时");
+        datas.add(20.9);
+        jsonObject.put("datas", datas);
+        try {
+            TestObject myJsonObject = objectMapper.readValue(jsonObject.toString(), TestObject.class);
+            System.out.println("解析后的Java对象： " + myJsonObject);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    // 测试用例-> 数组类型的Json字符串解析
+    public void test2(){
+        String jsonArrayStr = "[\"1\",\"2\",\"3\"]";
+        String json
+                = "[{\r\n"
+                + "\"name\" : \"Jack Ryan\",\r\n"
+                + "\"id\" : \"2019071075\",\r\n"
+                + "\"school\" : \"St. Jude's School\",\r\n"
+                + "\"section\" : \"B\"\r\n"
+                + "}]";
+        List<Student> list = JsonArrayStrToJavaObjectList(json, Student.class);
+        for (Object o : list) {
+            System.out.println("类型：" + o.getClass().getName());
+            System.out.println("值：" + o);
+        }
+    }
 
+    /**
+     * JsonArray类型字符串转为Java对象集合
+     * @param jsonArrayStr json字符串
+     * @param tClass 对象类型
+     * @param <T> 定义对象泛型
+     * @return
+     */
+    public static  <T> List<T> JsonArrayStrToJavaObjectList(String jsonArrayStr, Class<T> tClass){
+        return JSONArray.parseArray(jsonArrayStr, tClass);
+    }
     /**
      * 转换Json到指定的Java对象-------待验证
      * @param tClass
@@ -91,5 +139,16 @@ class Student {
         return "Student{"
                 + "name='" + name + '\'' + ", id='" + id + '\''
                 + ", school='" + school + '\'' + '}';
+    }
+}
+class TestObject{
+    List<Object> datas;
+
+    public List<Object> getDatas() {
+        return datas;
+    }
+
+    public void setDatas(List<Object> datas) {
+        this.datas = datas;
     }
 }
